57430808fd60b8e4cc433ef42dadfc138fc74376
buaazp <ziputao1165@gmail.com>
2013-07-26 03:14:45 +0800

    mv upload page to test directory.

--- gcc.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-gcc -g -levent -levent_openssl -levent_pthreads -lssl -lcrypto -levhtp -lMagickWand-Q16 -lmemcached  zhttpd.c zspinlock.c zlog.c zmd5.c zutil.c zcache.c zimg.c main.c -o main
-#for linux
-gcc -levhtp -levent -levent_openssl -levent_pthreads -lssl -lcrypto -lMagickWand -lmemcached  zhttpd.c zspinlock.c zlog.c zmd5.c zutil.c zcache.c zimg.c main.c -o main
